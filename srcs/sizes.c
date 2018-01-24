153a2cd3325e8ff3450df7cdde47722e1b2d9acd
erikagreen7777 <erikagreen7777@gmail.com>
2018-01-23 16:43:30 -0800

    normed sizes

--- srcs/sizes.c
+++ srcs/sizes.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sizes.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egreen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/23 16:36:33 by egreen            #+#    #+#             */
+/*   Updated: 2018/01/23 16:43:01 by egreen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../fdf.h"
 
 void	twenty(t_data *raw, t_coord **data)
@@ -14,9 +26,11 @@ void	twenty(t_data *raw, t_coord **data)
 	while (y < raw->width)
 	{
 		data[x][y].z = ft_atoi(raw->str[y]);
-		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * 20)) + ((600) - (raw->width * 5));
-		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * cos(ZANG) * cos(XANG) - \
-			(data[x][y].z) * sin(XANG)) * 5) + ((500) - ((raw->x * 10) - raw->x));
+		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * \
+					20)) + ((600) - (raw->width * 5));
+		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x *\
+					cos(ZANG) * cos(XANG) - (data[x][y].z) \
+					* sin(XANG)) * 5) + ((500) - ((raw->x * 10) - raw->x));
 		y++;
 	}
 	while (raw->str[++i])
@@ -38,9 +52,11 @@ void	fifty(t_data *raw, t_coord **data)
 	while (y < raw->width)
 	{
 		data[x][y].z = ft_atoi(raw->str[y]);
-		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * 20)) + ((950) - (raw->width * 8 ) / 2);
-		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * cos(ZANG) * cos(XANG) - \
-			(data[x][y].z) * sin(XANG)) * 20) + ((350) - ((raw->x * 8) - raw->x) / 2);
+		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) *\
+					20)) + ((950) - (raw->width * 8) / 2);
+		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * \
+					cos(ZANG) * cos(XANG) - (data[x][y].z) *\
+					sin(XANG)) * 20) + ((350) - ((raw->x * 8) - raw->x) / 2);
 		y++;
 	}
 	while (raw->str[++i])
@@ -48,7 +64,7 @@ void	fifty(t_data *raw, t_coord **data)
 	x++;
 }
 
-void 	mars(t_data *raw, t_coord **data)
+void	mars(t_data *raw, t_coord **data)
 {
 	int				i;
 	int				y;
@@ -62,9 +78,11 @@ void 	mars(t_data *raw, t_coord **data)
 	while (y < raw->width)
 	{
 		data[x][y].z = ft_atoi(raw->str[y]);
-		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * 8)) + ((1250) - (raw->width * 4) / 2);
-		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * cos(ZANG) * \
-			cos(XANG) - (data[x][y].z) * sin(XANG)) * 8) + ((300) - ((raw->x * 4) ) / 2);
+		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * \
+					8)) + ((1250) - (raw->width * 4) / 2);
+		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x *\
+					cos(ZANG) * cos(XANG) - (data[x][y].z) * \
+					sin(XANG)) * 8) + ((300) - ((raw->x * 4)) / 2);
 		y++;
 	}
 	while (raw->str[++i])
@@ -72,7 +90,7 @@ void 	mars(t_data *raw, t_coord **data)
 	x++;
 }
 
-void 	pees(t_data *raw, t_coord **data)
+void	pees(t_data *raw, t_coord **data)
 {
 	int				i;
 	int				y;
@@ -86,9 +104,11 @@ void 	pees(t_data *raw, t_coord **data)
 	while (y < raw->width)
 	{
 		data[x][y].z = ft_atoi(raw->str[y]);
-		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * 40)) + ((600) - (raw->width * 10));
-		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * cos(ZANG) * \
-			cos(XANG) - (data[x][y].z) * sin(XANG)) * 40) + ((400) - ((raw->x * 10) - raw->x));
+		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) \
+					* 40)) + ((600) - (raw->width * 10));
+		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * \
+					cos(ZANG) * cos(XANG) - (data[x][y].z) * \
+					sin(XANG)) * 40) + ((400) - ((raw->x * 10) - raw->x));
 		y++;
 	}
 	while (raw->str[++i])
@@ -110,9 +130,11 @@ void	pyra(t_data *raw, t_coord **data)
 	while (y < raw->width)
 	{
 		data[x][y].z = ft_atoi(raw->str[y]);
-		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * 20)) + ((800) - (raw->width * 10));
+		data[x][y].x = (((y * cos(ZANG) - x * sin(ZANG)) * \
+					20)) + ((800) - (raw->width * 10));
 		data[x][y].y = ((y * sin(ZANG) * cos(XANG) + x * cos(ZANG) * \
-			cos(XANG) - (data[x][y].z) * sin(XANG)) * 20) + ((500) - ((raw->x * 10) - raw->x));
+			cos(XANG) - (data[x][y].z) * sin(XANG)) * 20) \
+			+ ((500) - ((raw->x * 10) - raw->x));
 		y++;
 	}
 	while (raw->str[++i])
